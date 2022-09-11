26243eefe1c6e2dfb026fe23b40bd7f112665542
Radek Bednarik <radek.bednarik@tesena.com>
2022-09-11 14:34:16 +0200

    _

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,4 @@ import { Incidence } from "./api/incidence";
 import { Infected } from "./api/infected";
 import { Vaccination } from "./api/vaccination";
 
-// export const hospitalization = Hospitalization;
-// export const incidence = Incidence;
-// export const infected = Infected;
-// export const vaccination = Vaccination;
 export { Hospitalization, Incidence, Infected, Vaccination };
