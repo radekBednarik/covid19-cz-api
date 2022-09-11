a6ffadf35a369f3f47ea3e3f87cbaab2100cc0ed
Radek Bednarik <radek.bednarik@tesena.com>
2022-09-11 13:57:23 +0200

    added docstrings

--- index.js
+++
@@ -1,5 +0,0 @@
-import { Hospitalization } from "./src/api/hospitalization";
-import { Incidence } from "./src/api/incidence";
-import { Infected } from "./src/api/infected";
-import { Vaccination } from "./src/api/vaccination";
-export default { Hospitalization, Incidence, Infected, Vaccination };
